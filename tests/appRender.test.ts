d3a39af6be24ebf8388ff31fce7f2cdbf567f334
rcheruiyot <rcheruiyot@zamara.co.ke>
2024-08-19 11:21:28 +0300

    Cleanup

---
+++ tests/appRender.test.ts
@@ -0,0 +1,12 @@
+// import { render } from '@testing-library/react-native';
+// import HomeScreen from '../(tabs)';
+
+// describe('<HomeScreen />', () => {
+//     test('Text renders correctly on HomeScreen', () => {
+//       const { getByText } = render(<HomeScreen />);
+  
+//       const textLabel = getByText('Review');
+  
+//       expect(textLabel).toBeTruthy();
+//     });
+//   });
